e36b31ef7cf764cec2042881a7c7b2029fc4aafa
abrilmaguicha <abrumaguicha@gmail.com>
2020-08-27 23:44:27 -0300

    commit

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
 router.get('/users', (req, res) => {
-res.send('todos los usuarios');
+User.find((err, users) => {
+if (err) throw err;
+res.status(200).json(users);
+});
 });
 router.get('/user/:id', (req, res) => {
-res.send('el usuario con id ' + req.params.id);
+User.findOne({ id: req.params.id }, (err, user) => {
+if (err) throw err;
+res.status(200).json(user);
+});
 });
 module.exports = router;
