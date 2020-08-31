53247f4918f68a09d05b550e15ab2d7cf9520e69
abrilmaguicha <67296661+abrilmaguicha@users.noreply.github.com>
2020-08-31 16:25:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const router = require('./routes/api/user');
 const port = process.env.PORT || 3000;
 const db = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
-const app = express();
+
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
 mongoose
@@ -15,8 +15,8 @@ mongoose
 console.log(`DB connected @ ${db}`);
 })
 .catch(err => console.error(`Connection error ${err}`));
-app.use('/api', router);
 app.use(cors());
+app.use('/api', router);
 app.listen(port, () => {
 console.log(`Server listening on port ${port}`);
 });
